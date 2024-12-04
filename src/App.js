3df7ebdee16f7d60696f576dce5725639e5b9bb4
aldeepakraj321 <aldeepakraj321@gmail.com>
2024-12-04 18:30:55 +0530

    updated app.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Welcome to React World
+          Welcome to React
         </a>
       </header>
     </div>
